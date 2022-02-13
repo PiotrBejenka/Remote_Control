b6b254abe8ccd6ce9cf951fcc14f253161e2af72
PiotrBejenka <40249412+PiotrBejenka@users.noreply.github.com>
2022-02-14 00:24:02 +0100

    Add files via upload

--- platform/main.py
+++ platform/main.py
@@ -1,10 +1,6 @@
-import usys
-import ustruct as struct
-import utime
+import ustruct
 from machine import Pin, SPI, PWM
 from nrf24l01 import NRF24L01
-from micropython import const
-
 
 PWM_A = PWM(Pin(8))
 A_IN_1 = Pin(10, Pin.OUT)
@@ -74,49 +70,32 @@ def Stop():
     PWM_B.duty_u16(0)
     
     STBY.value(0)
-    
-    
-_RX_POLL_DELAY = const(15)
-
-_SLAVE_SEND_DELAY = const(10)
 
 pipes = (b"\xe1\xf0\xf0\xf0\xf0", b"\xd2\xf0\xf0\xf0\xf0")
 
-def slave():
-    csn = Pin(14, mode=Pin.OUT, value=1)
-    ce = Pin(17, mode=Pin.OUT, value=0)
+csn = Pin(14, mode=Pin.OUT, value=1)
+ce = Pin(17, mode=Pin.OUT, value=0)
     
-    nrf = NRF24L01(SPI(0), csn, ce, payload_size=8)
+nrf = NRF24L01(SPI(0), csn, ce, payload_size=8)
+nrf.open_tx_pipe(pipes[1])
+nrf.open_rx_pipe(1, pipes[0])
+nrf.start_listening()
 
-    nrf.open_tx_pipe(pipes[1])
-    nrf.open_rx_pipe(1, pipes[0])
-    nrf.start_listening()
+
+while True:
     
-    while True:
-        if nrf.any():
-            while nrf.any():
-                buf = nrf.recv()
-                dir, = struct.unpack("i", buf)
+    while nrf.any():
+        
+        buf = nrf.recv()
+        dir, = ustruct.unpack("i", buf)
                 
-                if dir == 0:
-                    Stop()
-                if dir == 8:
-                    Forward()
-                if dir == 2:
-                    Backward()
-                if dir == 4:
-                    Left()
-                if dir == 6:
-                    Right()
-                    
-                utime.sleep_ms(_RX_POLL_DELAY)
-
-            utime.sleep_ms(_SLAVE_SEND_DELAY)
-            nrf.stop_listening()
-            try:
-                nrf.send(struct.pack("i", dir))
-            except OSError:
-                pass
-            nrf.start_listening()
- 
-slave()
+        if dir == 0:
+            Stop()
+        if dir == 8:
+            Forward()
+        if dir == 2:
+            Backward()
+        if dir == 4:
+            Left()
+        if dir == 6:
+            Right()
